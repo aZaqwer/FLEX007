0f4dcc8663eb87191f88cbe4b567a55ef7fdd8e0
Alexandr Khvostikov <47623058+aZaqwer@users.noreply.github.com>
2021-06-30 13:39:52 +0300

    'update'

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,6 +1,6 @@
-$('.filter__item').click(function (event) {
-    var i = $(this).data('filter');
-    console.log(i)
+// $('.filter__item').click(function (event) {
+//     var i = $(this).data('filter');
+//     console.log(i)
 
-    return false;
-});
+//     return false;
+// });
